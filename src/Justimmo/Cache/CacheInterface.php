ffd68fe0529a4bf5c6c9eff806cdf30bb03002cb
Tschebel <simon.mitterhofer@gmail.com>
2013-12-09 14:00:49 +0100

    add justimmo cache

---
+++ src/Justimmo/Cache/CacheInterface.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Justimmo\Cache;
+
+interface CacheInterface
+{
+    /**
+     * returns the cache of a key of a key or false if key is not found
+     *
+     * @param string $key
+     *
+     * @return mixed
+     */
+    public function get($key);
+
+    /**
+     * adds a cache key for a specific lifetime into the cache
+     *
+     * @param string $key
+     * @param mixed $value
+     */
+    public function set($key, $value);
+
+    /**
+     * removes a key entry from the cache
+     *
+     * @param $key
+     */
+    public function remove($key);
+}
---
+++ src/Justimmo/Cache/NullCache.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Justimmo\Cache;
+
+/**
+ * Class NullCache
+ *
+ * Class Implements the Cache Interface but does not caches items
+ *
+ * @package Justimmo\Cache
+ */
+class NullCache implements CacheInterface
+{
+    /**
+     * returns the cache of a key of a key or false if key is not found
+     *
+     * @param string $key
+     *
+     * @return mixed
+     */
+    public function get($key)
+    {
+        return false;
+    }
+
+    /**
+     * adds a cache key for a specific lifetime into the cache
+     *
+     * @param string $key
+     * @param mixed $value
+     */
+    public function set($key, $value)
+    {
+
+    }
+
+    /**
+     * removes a key entry from the cache
+     *
+     * @param $key
+     */
+    public function remove($key)
+    {
+
+    }
+}
