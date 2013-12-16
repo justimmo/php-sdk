b02729a8bc966e2697883062b605751f9e3f3b8b
Tschebel <simon.mitterhofer@gmail.com>
2013-12-16 16:07:23 +0100

    add NullMapper

---
+++ src/Justimmo/Model/Mapper/NullMapper.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Justimmo\Model\Mapper;
+
+class NullMapper implements MapperInterface
+{
+    /**
+     * gets the setter for a api property name
+     *
+     * @param $apiPropertyName
+     *
+     * @return string
+     */
+    public function getSetter($apiPropertyName)
+    {
+        return $apiPropertyName;
+    }
+
+    /**
+     * get the type a property has to be cast ing
+     *
+     * @param $apiPropertyName
+     *
+     * @return string
+     */
+    public function getType($apiPropertyName)
+    {
+        return $apiPropertyName;
+    }
+
+    /**
+     * get the name of the property on a model object for a api property name
+     *
+     * @param $apiPropertyName
+     *
+     * @return string
+     */
+    public function getProperty($apiPropertyName)
+    {
+        return $apiPropertyName;
+    }
+
+    /**
+     * gets a filter property name to sent to the api call for a specific model property
+     *
+     * @param $modelPropertyName
+     *
+     * @return string
+     */
+    public function getFilterPropertyName($modelPropertyName)
+    {
+        return $modelPropertyName;
+    }
+}
