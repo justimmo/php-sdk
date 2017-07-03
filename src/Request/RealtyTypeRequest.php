e1426aab03b74448c5fca8a95a12f6962fdc7c84
Tschebel <simon.mitterhofer@gmail.com>
2017-07-03 11:31:46 +0200

    Add available filters to RealtyTypeRequest and SubRealtyTypeRequest

--- src/Request/RealtyTypeRequest.php
+++ src/Request/RealtyTypeRequest.php
@@ -4,9 +4,16 @@ namespace Justimmo\Api\Request;
 
 use Justimmo\Api\Entity\RealtyType;
 
+/**
+ * @method $this filterByWithRealties($value)
+ */
 class RealtyTypeRequest extends ApiRequest
 {
     const FIELD_SUB_REALTY_TYPES = 'subRealtyTypes';
+
+    const FILTERS = [
+        'withRealties',
+    ];
 
     /**
      * @inheritDoc
--- src/Request/SubRealtyTypeRequest.php
+++ src/Request/SubRealtyTypeRequest.php
@@ -4,9 +4,16 @@ namespace Justimmo\Api\Request;
 
 use Justimmo\Api\Entity\SubRealtyType;
 
+/**
+ * @method $this filterByWithRealties($value)
+ */
 class SubRealtyTypeRequest extends ApiRequest
 {
     const FIELD_REALTY_TYPE = 'realtyType';
+
+    const FILTERS = [
+        'withRealties',
+    ];
 
     /**
      * @inheritDoc
