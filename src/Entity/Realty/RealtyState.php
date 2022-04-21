6e1f0aaef623d792b46231e8dcc164c9e4170d4b
19Gerhard85 <gerhard.mikysa@gmail.com>
2022-04-21 13:59:56 +0200

    [Realty State] Added new realty state "contract_preparation"

--- src/Entity/Realty/RealtyState.php
+++ src/Entity/Realty/RealtyState.php
@@ -14,14 +14,14 @@ class RealtyState implements Entity
 {
     use Identifiable, Nameable;
 
-    const STATUS_DRAFT             = 4;
-    const STATUS_ACTIVE            = 5;
-    const STATUS_INACTIVE          = 6;
-    const STATUS_RESERVED          = 7;
-    const STATUS_PROCURED          = 8;
-    const STATUS_CANCELED          = 9;
-    const STATUS_EXTERNAL_PROCURED = 10;
-
+    const STATUS_DRAFT                = 4;
+    const STATUS_ACTIVE               = 5;
+    const STATUS_INACTIVE             = 6;
+    const STATUS_RESERVED             = 7;
+    const STATUS_CONTRACT_PREPARATION = 11;
+    const STATUS_PROCURED             = 8;
+    const STATUS_CANCELED             = 9;
+    const STATUS_EXTERNAL_PROCURED    = 10;
 
     public function __toString()
     {
