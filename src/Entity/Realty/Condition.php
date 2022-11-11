d2a85534a39787633da39bd623c6f1cbee96a01f
zoltan-sule <z.sule@justimmo.at>
2022-11-11 18:13:19 +0100

    Adds new filter to realty request
    Implements
     * filter by condition
    
    Ref: JI-1683

--- src/Entity/Realty/Condition.php
+++ src/Entity/Realty/Condition.php
@@ -14,6 +14,23 @@ class Condition implements Entity
 {
     use Identifiable, Nameable;
 
+    public const FIRST_TIME_OCCUPANCY     = 'first_time_occupancy'; // ERSTBEZUG
+    public const IN_NEED_OF_RENOVATION    = 'in_need_of_renovation'; // RENOVIERUNGSBEDUERFTIG
+    public const IN_MINT_CONDITION        = 'in_mint_condition'; // NEUWERTIG
+    public const COMPLETELY_REFURBISHED   = 'completely_refurbished'; // VOLLSANIERT
+    public const COMPLETELY_RENOVATED     = 'completely_renovated'; // VOLLRENOVIERT
+    public const DILAPIDATED              = 'dilapidated'; // BAUFAELLIG
+    public const AFTER_AGREEMENT          = 'after_agreement'; // NACH_VEREINBARUNG
+    public const MODERNIZED               = 'modernized'; // MODERNISIERT
+    public const REFINED                  = 'refined'; // GEPFLEGT
+    public const SHELL                    = 'shell'; // ROHBAU
+    public const GUTTED                   = 'gutted'; // ENTKERNT
+    public const PROPERTY_FOR_DEMOLITION  = 'property_for_demolition'; // ABRISSOBJEKT
+    public const PLANNED                  = 'planned'; // PROJEKTIERT
+    public const PARTIALLY_REFURBISHED    = 'partially_refurbished'; // TEILSANIERT
+    public const PARTIALLY_RENOVATED      = 'partially_renovated'; // TEILRENOVIERT
+    public const IN_NEED_OF_REFURBISHMENT = 'in_need_of_refurbishment'; // SANIERUNGSBEDUERFTIG
+
     public function __toString()
     {
         return (string) $this->getName();
